45e8a27d762747fd226ae69452aa42522946f00b
Nelson <Nelsonshadow@yahoo.es>
2017-12-21 19:14:02 +0100

    Prescripciones ADD/FILTRO

--- src/main/java/es/uvigo/esei/dagss/controladores/medico/MedicoControlador.java
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/MedicoControlador.java
@@ -25,6 +25,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar;
+import org.primefaces.event.SelectEvent;
 
 
 /**
@@ -56,7 +57,7 @@ public class MedicoControlador implements Serializable {
     private MedicoDAO medicoDAO;
     @EJB
     private MedicamentoDAO medicamentoDAO;
-    private List<Medicamento> selectedMeds;
+    private Medicamento selectedMed;
     /**
      * Creates a new instance of AdministradorControlador
      */
@@ -210,25 +211,31 @@ public class MedicoControlador implements Serializable {
     }
     
     public String addPrescripcion() throws ParseException{
+        System.out.println("======= SALIDA TESTING ======== ");
+        System.out.println("Dosis: " + prescripcion.getDosis());
+        System.out.println("Indicaciones: " + prescripcion.getIndicaciones());
+        System.out.println("F.Inicio: " + prescripcion.getFechaInicio());
+        System.out.println("F.Fin: " + prescripcion.getFechaFin());
         prescripcion.setMedico(medicoActual);
         prescripcion.setPaciente(citaDetalle.getPaciente());
-        
+        prescripcion.setMedicamento(selectedMed);
         medicoDAO.anhadirPrescripcion(prescripcion);
         return doShowRecetas(citaDetalle.getPaciente());
     
     }
     
     public String doNuevaPrescripcion(){
+        prescripcion = new Prescripcion();
         setMedicamentos(medicamentoDAO.getAll());
         return "nuevaPrescripcion";
     }
     
-    public void setSelectedMeds(List<Medicamento> selectedMeds) {
-        this.selectedMeds = selectedMeds;
+    public void setSelectedMed(Medicamento selectedMed) {
+        this.selectedMed = selectedMed;
     }
     
-    public List<Medicamento> getSelectedMeds() {
-        return this.selectedMeds;
+    public Medicamento getSelectedMed() {
+        return this.selectedMed;
     }
     
     public void doBorrarPrescripcion(Prescripcion prescripcion){
@@ -237,5 +244,11 @@ public class MedicoControlador implements Serializable {
     
     public void doEditarPrescripcion(Prescripcion prescripcion){
         medicoDAO.actualizarPrescripcion(prescripcion);
+    }
+
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
     }
 }
